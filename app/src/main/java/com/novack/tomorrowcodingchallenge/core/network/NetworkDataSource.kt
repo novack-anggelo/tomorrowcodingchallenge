ba1e4433d7a6f08c44540651a5ecc16452231250
novack-anggelo <anggelo.novack@gmail.com>
2023-11-15 11:25:03 +0100

    #3: removed solved TODO

--- app/src/main/java/com/novack/tomorrowcodingchallenge/core/network/NetworkDataSource.kt
+++ app/src/main/java/com/novack/tomorrowcodingchallenge/core/network/NetworkDataSource.kt
@@ -3,5 +3,5 @@ package com.novack.tomorrowcodingchallenge.core.network
 import com.novack.tomorrowcodingchallenge.core.network.model.WeatherResponseDTO
 
 interface NetworkDataSource {
-    suspend fun getWeatherData(lat: String, long: String): WeatherResponseDTO // TODO change to domain model
+    suspend fun getWeatherData(lat: String, long: String): WeatherResponseDTO
 }
